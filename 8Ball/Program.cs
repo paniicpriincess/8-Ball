1b2ca655465faffad62b2ab182948e3652c38393
paniicpriincess <129639367+paniicpriincess@users.noreply.github.com>
2023-04-02 10:33:21 -0400

    Answer user's question with random number generator

---
+++ 8Ball/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _8Ball
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Run infinitely until user closes program
+            while (true)
+            {
+                // Get random number between 0 and 4
+                Random rng = new Random();
+                int randomNumber = rng.Next(0, 4);
+
+                // Have user ask question
+                Console.Write("Ask the 8-Ball a yes or no question: ");
+                Console.ReadLine();
+
+                // Have the 8-Ball answer the question
+                Console.Write("Answer: ");
+                switch(randomNumber)
+                {
+                    case 0:
+                        Console.Write("Yes.\n");
+                        break;
+                    case 1:
+                        Console.Write("No.\n");
+                        break;
+                    case 2:
+                        Console.Write("Maybe.\n");
+                        break;
+                    case 3:
+                        Console.Write("Try again later.\n");
+                        break;
+                    default:
+                        // Random number must be 4
+                        Console.Write(";)\n");
+                        break;
+                }
+            }
+        }
+    }
+}
